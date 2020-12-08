43412edbcbb64a290bbdeb59760d189ef5712e8c
Ivan Arar <ivan.arar@gmail.com>
2020-12-08 15:38:23 +0100

    pyyaml>=5.3.1

--- setup.py
+++ setup.py
@@ -28,7 +28,7 @@ def get_install_requires():
     res.append('boto3>=1.9.142')
     res.append('requests_aws4auth>=0.9')
     res.append('click>=6.7,<=7.1.2')
-    res.append('pyyaml==3.13')
+    res.append('pyyaml>=5.3.1')
     res.append('voluptuous>=0.9.3')
     res.append('certifi>=2019.9.11')
     res.append('six>=1.11.0')
