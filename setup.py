bacaedc5ceb9c83a4f97e65ac12d50a35977cd1e
Ivan Arar <ivan.arar@gmail.com>
2020-12-08 15:22:57 +0100

    elasticsearch 7.7.0

--- setup.py
+++ setup.py
@@ -22,7 +22,7 @@ def get_version():
     return VERSION
 
 def get_install_requires():
-    res = ['elasticsearch==7.1.0' ]
+    res = ['elasticsearch==7.7.0' ]
     res.append('urllib3<1.26,>=1.25.4')
     res.append('requests>=2.20.0')
     res.append('boto3>=1.9.142')
